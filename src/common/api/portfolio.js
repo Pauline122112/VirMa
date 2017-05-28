ee27f5d413bc058bcc0ae80837e86ae6a23cadad
Pauline <pauline.design007@gmail.com>
2017-05-28 13:36:35 +1200

    playing with function

--- src/common/api/portfolio.js
+++ src/common/api/portfolio.js
@@ -2,17 +2,6 @@ export function getPortfolio() {
   return [
 
     {
-    	link : 'http://www.covestor.com',
-    	title : 'Covestor',
-    	classname : 'covestor',
-    	roles : [
-    		{
-    			title : 'Front End Developer',
-    			skills : 'OO Javascript, CSS3, HTML5'
-    		}
-    	]
-    },
-    {
     	link : 'http://www.shipserv.com',
     	title : 'ShipServ',
     	classname : 'shipserv',
