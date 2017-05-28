89dad105df760c7105f25d6be0c629ca6820b6ff
Pauline <pauline.design007@gmail.com>
2017-05-28 13:33:54 +1200

    deleted material from portfolio.js

--- src/common/api/portfolio.js
+++ src/common/api/portfolio.js
@@ -1,20 +1,6 @@
 export function getPortfolio() {
   return [
-    {
-    	link : 'http://www.goldmansachs.com',
-    	title : 'Goldman Sachs',
-    	classname : 'goldmans',
-    	roles : [
-    		{
-    			title : 'Front End Developer',
-    			skills : 'React, Backbone, D3'
-    		},
-    		{
-    			title : 'Back End Developer',
-    			skills : 'Node, MongoDB, Webpack'
-    		}
-    	]
-    },
+  
     {
     	link : 'http://www.libon.com/',
     	title : 'Orange (Libon)',
