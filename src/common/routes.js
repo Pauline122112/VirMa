cb778d7bf61f379c634d3947ac951d819e3f9297
Pauline <pauline.design007@gmail.com>
2017-05-30 09:48:48 +1200

    edited buying keyword in routes.js

--- src/common/routes.js
+++ src/common/routes.js
@@ -9,14 +9,14 @@ import HomePage from "./containers/HomePage";
 
 //Redux Dumb
 import SellerPage from "./components/Seller";
-import ServicesPage from "./components/Services";
+import BuyingPage from "./components/Buying";
 import error404 from "./components/404";
 
 export default (
   <Route name="app" path="/" component={App}>
       <Route path="home" component={HomePage} />
       <Route path="seller" component={SellerPage} />
-      <Route path="services" component={ServicesPage} />
+      <Route path="buying" component={BuyingPage} />
       <Route path="about" component={AboutPage} />
       <Route path="*" component={error404}/>
   </Route>
